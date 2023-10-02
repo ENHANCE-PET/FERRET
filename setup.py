346e0186ea027bac7f129fef810f761f985e956c
nutellaBear <48599863+LalithShiyam@users.noreply.github.com>
2023-10-02 15:22:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,30 @@ from setuptools import setup, find_packages
 setup(
     name='ferretz',
     version='0.1',
+    description='A Python package to create "enhance.pet" compliant package folder structures',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Lalith Kumar Shiyam Sundar',
+    author_email='lalith.shiyamsundar@meduniwien.ac.at',
+    url='https://github.com/LalithShiyam/ferretz',  # Replace with your GitHub URL
+    license='MIT',  # Replace with your license
     packages=find_packages(),
     entry_points={
         'console_scripts': ['ferretz=ferretz.ferretz:main'],
     },
-    # other metadata and requirements
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    keywords='enhance.pet, package creation, Python',
+    install_requires=[
+        # List your package dependencies here
+    ],
 )
